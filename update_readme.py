7e0e87f5e208ea5f6762d6bec43d087fbbffcb47
Ivan <vanzag1738@ya.ru>
2025-04-30 21:48:35 +0000

    added README updater

---
+++ update_readme.py
@@ -0,0 +1,38 @@
+from generate_stats.lang_generator import get_languages
+from generate_stats.editors_OS_generator import get_editors_and_os
+from generate_stats.info_generator import get_time_range, get_total_time, get_daily_average
+from generate_stats.waka_api import editors, languages, operating_systems, total_time, daily_average
+
+
+readme_page = (
+    f'```go\n'
+    f'{get_time_range()}\n'
+	f'{get_daily_average(daily_average)}'
+	f'{get_total_time(total_time)}\n\n'
+	f'{get_languages(languages)}\n\n'
+	f'{get_editors_and_os(editors, operating_systems)}'
+	f'```'
+)
+
+
+if __name__ == '__main__':
+    start_marker = "<!--START_SECTION:waka-->"
+    end_marker = "<!--END_SECTION:waka-->"
+
+    with open("README.md", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    start = content.find(start_marker)
+    end = content.find(end_marker)
+
+    if start == -1 or end == -1:
+        raise ValueError("Markers not found in README.md")
+
+    updated = (
+        content[:start + len(start_marker)] +
+        "\n\n" + readme_page + "\n\n" +
+        content[end:]
+    )
+
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write(updated)
